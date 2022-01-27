ee41293f34691b79bd3dd15555893d3a61e7045e
devjun63 <59526539+devjun63@users.noreply.github.com>
2022-01-28 00:41:28 +0900

    First Commit index.html 생성 application.properties에서 port 8002로 변경

---
+++ src/main/java/com/familyevents/FamilyeventsApplication.java
@@ -0,0 +1,13 @@
+package com.familyevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/familyevents/FamilyeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
